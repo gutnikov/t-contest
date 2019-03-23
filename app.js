07a4d7edc114f635b850b5343c010b56f487c963
a.gutnikov <a.gutnikov@corp.badoo.com>
2019-03-23 15:56:07 +0300

    buttons

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 // TODO:
-// Vertical ruler showing current position
-
-// blinking numbers
-// Buttons click and show bug
 // Range boundaries not always goes to 0
 // Mouse variant exceeds min/max area
 // Canvas resize breaks ruler
+// blinking numbers
 
 // Use same colors as on demo (themes)
 // Night mode for a page
@@ -47,7 +44,7 @@ class ChartCanvas {
         // Canvas
         this.canvas = createCanvas(width, height);
         this.context2d = this.canvas.getContext('2d');
-        this.plotAreaPadding = this.hasRulers ? v2(0, 20 * getDpr()) : v2(0, 0);
+        this.plotAreaPadding = this.hasRulers ? v2(0, 25 * getDpr()) : v2(0, 0);
         this.plotArea = v2(
             this.canvas.width - this.plotAreaPadding.x * 2,
             this.canvas.height - this.plotAreaPadding.y * 2);
@@ -58,6 +55,7 @@ class ChartCanvas {
 
         this.p0 = p0;
         this.p1 = p1;
+        this.iHover = null;
 
         this.setFromPct(p0, p1);
         this.setFromPct(p0, p1);
@@ -72,9 +70,32 @@ class ChartCanvas {
         this.timings = {};
         this.animations = [];
 
-		this.lastUpdate = Date.now();
+        this.lastUpdate = Date.now();
+        this.setEvents();
         this.update = this.update.bind(this);
         this.update();
+    }
+
+    setEvents() {
+        this.canvas.addEventListener('mousemove', function(e) {
+            const rect = this.canvas.getBoundingClientRect();
+            const cords = v2((e.clientX - rect.left) * getDpr(),
+                (e.clientY - rect.top) * getDpr());
+            const anyLineName = Object.keys(this.lines)[0];
+            const anyLine = this.plotPoints[anyLineName];
+            const delta = (anyLine[2][1].x - anyLine[1][1].x)/2;
+            const hInd = anyLine.findIndex(function(ip) {
+                if (ip[0] === null) {
+                    return false;
+                }
+                return ip[1].x >= cords.x - delta && ip[1].x <= cords.x + delta;
+            });
+            this.iHover = hInd !== -1 ? hInd : null;
+        }.bind(this));
+
+        this.canvas.addEventListener('mouseout', function(e) {
+            this.iHover = null;
+        }.bind(this));
     }
 
     setSize(w, h) {
@@ -114,10 +135,72 @@ class ChartCanvas {
         this.x1 = _1[0];
         this.i1 = _1[1];
 
-        this.sourceHeight = this.getMaxHeight();
+        this.sourceHeight = this.getMaxHeight(this.prevSourceHeight);
         this.sourceOffset = v2(this.x0, 0);
         this.sourceArea = v2(this.x1 - this.x0, this.sourceArea ? this.sourceArea.y : this.sourceHeight);
         this.factor = map(this.sourceArea, this.plotArea);
+    }
+
+    setPlotPoints() {
+        const self = this;
+        this.plotPoints = Object.keys(this.lines).reduce(function(all, name) {
+            all[name] = self.setLinePlotPoints(name);
+            return all;
+        }, {});
+    }
+
+    setLinePlotPoints(name) {
+        const points = [];
+        const xs = this.x.slice(this.i0, this.i1 + 1);
+        const ys = this.lines[name].slice(this.i0, this.i1 +1);
+
+        for (let i = 0; i < xs.length; i++ ) {
+            const plotPoint = add(invertY(
+                scale(
+                    sub(
+                        v2(xs[i], ys[i]),
+                        this.sourceOffset
+                    ),
+                    this.factor
+                ), this.plotArea), this.plotAreaPadding);
+            points.push([this.i0 + i, plotPoint]);
+        }
+
+        // First point
+        if (this.i0 > 0) {
+            points.unshift([null,
+                add(invertY(
+                    scale(
+                        sub(
+                            v2(this.x0, linePoint(
+                                v2(this.x[this.i0 - 1], this.lines[name][this.i0 - 1]),
+                                v2(this.x[this.i0], this.lines[name][this.i0]),
+                                this.x0)
+                                .y),
+                            this.sourceOffset
+                        ),
+                        this.factor
+                    ), this.plotArea), this.plotAreaPadding)
+            ]);
+        }
+        // Last point
+        if (this.i1 < this.lines[name].length - 2) {
+            points.push([null,
+                add(invertY(
+                    scale(
+                        sub(
+                            v2(this.x1, linePoint(
+                                v2(this.x[this.i1], this.lines[name][this.i1]),
+                                v2(this.x[this.i1+1], this.lines[name][this.i1+1]),
+                                this.x1
+                            ).y),
+                            this.sourceOffset
+                        ),
+                        this.factor
+                    ), this.plotArea), this.plotAreaPadding)
+            ]);
+        }
+        return points;
     }
 
     getXI(p) {
@@ -185,16 +268,17 @@ class ChartCanvas {
         return result;
     }
 
-    getMaxHeight() {
+    getMaxHeight(defaultValue) {
         const arrays = Object.keys(this.lines)
             .filter(function(key) {return this.linesEnabled[key]}, this)
             .map(function(key){ return this.lines[key]}, this);
-        return splitRange(0, arraysMaxValue(arrays, this.i0, this.i1), 6)[6];
+        return arrays.length ? splitRange(0, arraysMaxValue(arrays, this.i0, this.i1), 6)[6] : defaultValue;
     }
 
     update() {
 		// Update animation timings
         this.updateTimings();
+        this.setPlotPoints();
         if (this.inputChanged()) {
             this.setFromPct();
             // run animation
@@ -222,11 +306,12 @@ class ChartCanvas {
     }
 
     handleXRangeChanged() {
+        console.log(this.i0, this.i1, this.x0, this.x1);
         const wt = this.timing('changeWidth');
         if (!wt || wt() === 1) {
             this.timing('changeWidth', timing(500, this.handleWidthTimingDone.bind(this)));
         }
-        let newSteps;
+        let newSteps = [];
         for (let i = 0; i < this.stepsScale.length; i++) {
             let steps = this.stepsScale[i].filter(v => v >= this.i0 && v <= this.i1);
             if (steps.length > this.xRangeSteps) {
@@ -284,6 +369,7 @@ class ChartCanvas {
     }
 
     renderLine(name) {
+        const points = this.plotPoints[name];
         const t = this.timing('line:' + name);
         let alpha =  t ? t() : 1;
         this.context2d.beginPath();
@@ -292,46 +378,12 @@ class ChartCanvas {
             easeInOutQuart(this.linesEnabled[name] ? alpha : 1 - alpha)
         );
         this.context2d.lineWidth = 2.5 * getDpr();
-
-        // cords
-        const xs = this.x.slice(this.i0, this.i1 + 1);
-        const ys = this.lines[name].slice(this.i0, this.i1 +1);
-
-        // First point
-        if (this.i0 > 0) {
-            xs.unshift(this.x0);
-            ys.unshift(
-                linePoint(
-                    v2(this.x[this.i0-1], this.lines[name][this.i0-1]),
-                    v2(this.x[this.i0], this.lines[name][this.i0]),
-                    this.x0)
-                .y);
-        }
-
-        // Last point
-        if (this.i1 < this.lines[name].length - 2) {
-            xs.push(this.x1);
-            ys.push(
-                linePoint(
-                    v2(this.x[this.i1], this.lines[name][this.i1]),
-                    v2(this.x[this.i1+1], this.lines[name][this.i1+1]),
-                    this.x1
-                ).y);
-        }
-
-        for (let i = 0; i < xs.length; i++ ) {
-            const plotPoint = add(invertY(
-                scale(
-                    sub(
-                        v2(xs[i], ys[i]),
-                        this.sourceOffset
-                    ),
-                    this.factor
-                ), this.plotArea), this.plotAreaPadding);
+        for (let i = 0; i < points.length; i++ ) {
+            const p = points[i][1];
             if (i === 0) {
-                this.context2d.moveTo(plotPoint.x,plotPoint.y);
+                this.context2d.moveTo(p.x,p.y);
             } else {
-                this.context2d.lineTo(plotPoint.x,plotPoint.y);
+                this.context2d.lineTo(p.x,p.y);
             }
         }
         this.context2d.stroke();
@@ -351,9 +403,8 @@ class ChartCanvas {
         const v = scale(sub(v2(x, 0), this.sourceOffset), this.factor);
         this.context2d.font = "28px Arial";
         this.context2d.fillStyle = `rgba(0, 0, 0, ${alpha})`;
-        this.context2d.fillText(label, v.x, this.plotArea.y + this.plotAreaPadding.y);
+        this.context2d.fillText(label, v.x, this.plotArea.y + this.plotAreaPadding.y * 2 - 10);
     }
-
 
     renderYRulers() {
         const t = this.timing('changeHeight');
@@ -366,10 +417,9 @@ class ChartCanvas {
         this.context2d.beginPath();
         this.context2d.strokeStyle = `rgba(224, 224, 224, ${alpha})`;
         this.context2d.lineWidth = 1 * getDpr();
-        const rv = invertY(
+        const rv = add(invertY(
             scale(v2(0, y), this.factor
-            ), this.plotArea)
-
+            ), this.plotArea), this.plotAreaPadding);
         this.context2d.moveTo(20, rv.y) ;
         this.context2d.lineTo(this.plotArea.x, rv.y);
         this.context2d.stroke();
@@ -378,33 +428,36 @@ class ChartCanvas {
         this.context2d.fillText(String(Math.ceil(y)), 30, rv.y - 18);
     }
 
-    renderTooltip(i) {
-        const ctx = this.context2d;
-        const x = this.x[i];
-        const linePoints = Object.keys(this.lines).map(function(name){
-            const y = this.lines[name][i];
-            return [name, add(invertY(scale(
-                sub(
-                    v2(x, y),
-                    this.sourceOffset
-                ),
-                this.factor
-            ), this.plotArea), this.plotAreaPadding), this.lines[name][i]];
-        }, this);
+    renderTooltip() {
+        if (!Number.isFinite(this.iHover)) {
+            return;
+        }
+        const self = this;
+        const anyLine = Object.keys(this.lines)[0];
+        const points = Object.keys(this.lines).reduce(function(all, name){
+            const pp = self.plotPoints[name][self.iHover];
+            all[name] = {
+                p: pp[1],
+                value: self.lines[name][pp[0]],
+                date: self.xDates[pp[0]]
+            };
+            return all;
+        }, {});
+        const anyPoint = points[anyLine];
 
-        // Render line
-        const plotPoint = linePoints[0][1];
+        const ctx = this.context2d;
+        // Line
         ctx.beginPath();
         ctx.strokeStyle = `rgba(224, 224, 224)`;
         ctx.lineWidth = 1 * getDpr();
-        ctx.moveTo(plotPoint.x, 0) ;
-        ctx.lineTo(plotPoint.x, this.plotArea.y);
+        ctx.moveTo(anyPoint.p.x, 0) ;
+        ctx.lineTo(anyPoint.p.x, this.plotArea.y);
         ctx.stroke();
         ctx.lineWidth = 2 * getDpr();
-        // Render circles
-        linePoints.forEach(function(np) {
-            const name = np[0];
-            const p = np[1];
+
+        // Circles
+        Object.keys(points).forEach(function(name) {
+            const p = points[name].p;
             const t = this.timing('line:' + name);
             let alpha =  t ? t() : 1;
             ctx.fillStyle = 'rgba(255,255,255)';
@@ -418,13 +471,12 @@ class ChartCanvas {
             ctx.stroke();
         }, this);
 
-        // Render tooltip
+        // Tooltip
         const rectWidth = 200 * getDpr();
-        const rectHeight = 50 * getDpr() * (Math.ceil(linePoints.length/2) + 1);
-        const rectX = plotPoint.x - rectWidth * (1/3);
+        const rectHeight = 50 * getDpr() * (Math.ceil(Object.keys(this.lines).length/2) + 1);
+        const rectX = points[anyLine].p.x - rectWidth * (1/3);
         const rectY = 10;
         const cornerRadius = 20;
-
         ctx.strokeStyle = 'rgba(205,205,205)';
         // ctx.lineJoin = "round";
         // ctx.lineWidth = cornerRadius;
@@ -436,21 +488,19 @@ class ChartCanvas {
         // Header
         this.context2d.font = "28px Arial";
         this.context2d.fillStyle = `rgba(0, 0, 0, 1)`;
-        this.context2d.fillText(this.xDates[i], rectX + rectWidth/2 - 50, rectY + 50);
-    
-        linePoints.forEach(function(npv, i) {
-            const name = npv[0];
-            const label = this.names[name];
-            const v = npv[2];
+        this.context2d.fillText(anyPoint.date, rectX + rectWidth/2 - 50, rectY + 50);
+
+        Object.keys(points).forEach(function(name, i) {
+            const point = points[name];
             ctx.fillStyle = withAlpha(
                 this.rgbaColors[name], 1
             );
             ctx.font = "36px Arial";
             const itemY = rectY + 100 + Math.floor(i/2) * 100;
             const itemX = rectX + 50 + (i % 2) * 150;
-            ctx.fillText(String(v), itemX, itemY);
+            ctx.fillText(point.value, itemX, itemY);
             ctx.font = "28px Arial";
-            ctx.fillText(label, itemX, itemY + 34);
+            ctx.fillText(this.names[name], itemX, itemY + 34);
         }, this);
     }
 }
@@ -470,7 +520,7 @@ function chartAt(parent, data) {
     const width = rect.width;
     const height = width * (2/3);
     const mainCanvas = new ChartCanvas(width, height, data, true, p0, p1);
-    const rulerCanvas = new ChartCanvas(width, 50, data, false, p0, p1);
+    const rulerCanvas = new ChartCanvas(width, 50, data, false, 0, 1);
     const ruler = new Ruler({
         width: rect.width * getDpr(),
         height: 50 * getDpr(),
@@ -484,12 +534,10 @@ function chartAt(parent, data) {
         minMainArea: 0.05,
         touchAreaWidth: 30,
         onChange: function(min, max) {
-            mainCanvas.setRange(min , max );
+            mainCanvas.setRange(min , max);
         }
     });
-    const buttons = new Buttons(
-        Object.entries(window.data[0].names).map(function(e){ return {id: e[0], name: e[1]}}),
-    function(name, value) {
+    const buttons = new Buttons(data, function(name, value) {
         mainCanvas.setLineEnabled(name, value);
         rulerCanvas.setLineEnabled(name, value);
     });
@@ -498,15 +546,15 @@ function chartAt(parent, data) {
     element.querySelector('.chart-ruler').appendChild(rulerCanvas.canvas);
     element.querySelector('.chart-ruler').appendChild(ruler.canvas);
     element.querySelector('.chart-buttons').appendChild(buttons.element);
-    window.addEventListener('resize', function() {
-        const newRect = parent.getBoundingClientRect();
-        if (newRect.width !== rect.width) {
-            mainCanvas.setSize(newRect.width, newRect.width * 2/3);
-            rulerCanvas.setSize( newRect.width, 50);
-            ruler.canvas.width = newRect.width;
-            ruler.canvas.style = `width: ${rect.width}px; height: ${50}px`;
-        }
-    });
+    // window.addEventListener('resize', function() {
+    //     const newRect = parent.getBoundingClientRect();
+    //     if (newRect.width !== rect.width) {
+    //         mainCanvas.setSize(newRect.width, newRect.width * 2/3);
+    //         rulerCanvas.setSize( newRect.width, 50);
+    //         ruler.canvas.width = newRect.width;
+    //         ruler.canvas.style = `width: ${rect.width}px; height: ${50}px`;
+    //     }
+    // });
     parent.appendChild(element);
 }
 
@@ -516,57 +564,3 @@ const container = document.getElementById('container');
 window.data.forEach(function(data) {
     chartAt(container, data);
 });
-
-// // Bootstrap
-//
-// const whRatio = 2/3;
-// const containerRect = container.getBoundingClientRect();
-//
-// const main = new Chart(
-//     containerRect.width,
-//     containerRect.width * whRatio,
-//     window.data[0],
-//     true,
-//     0.3,
-//     0.6
-// );
-// const ruler = new Chart(
-//     containerRect.width,
-//     50,
-//     window.data[0],
-//     false,
-//     0.3,
-//     0.6
-// );
-//
-// const chart = document.getElementById('chart');
-// chart.appendChild(main.canvas);
-// chart.appendChild(ruler.canvas);
-// const slider = new Ruler({
-// 	width: containerRect.width * getDpr(),
-// 	height: 50 * getDpr(),
-// 	theme: {
-// 		mainColor: 'rgba(0,0,0,0.3)',
-// 		borderColor: 'rgba(0,0,0, 0.5)',
-// 		border: [1 * getDpr(), 6 * getDpr()]
-// 	},
-// 	left: 0.3,
-// 	right: 0.6,
-//     minMainArea: 0.05,
-// 	touchAreaWidth: 30,
-// 	onChange: function(min, max) {
-//     	main.setRange(min , max );
-// 	}
-// });
-// container.appendChild(slider.canvas);
-//
-// const buttons = new Buttons(
-//     Object.entries(window.data[0].names).map(function(e){ return {id: e[0], name: e[1]}}),
-//     function(name, value) {
-//         main.setLineEnabled(name, value);
-//         ruler.setLineEnabled(name, value);
-//     }
-//     );
-// buttons.mountTo(container);
-
-
--- buttons.js
+++ buttons.js
@@ -1,22 +1,56 @@
 class Buttons {
-    constructor(names, onChange) {
-        this.names = names;
+    constructor(data, onChange) {
+        const rnd = String(Math.random()).slice(-3);
+        this.buttons = Object.keys(data.names).reduce(function(all, name) {
+            all[name] = {
+                name: name,
+                title: data.names[name],
+                color: data.colors[name],
+                id: name + '-' + rnd
+            };
+            return all;
+        }, {});
         this.onChange = onChange || function(){};
         this.element = document.createElement('div');
-        this.element.innerHTML = names.map(function(item, i) {
-            return `
-                <div class="checkbox">
-                    <input type="checkbox" id="ch${i}" name="${item.id}" checked="true"/>
-                    <label for="ch${i}">${item.name}</label>
-                </div>
-            `;
-        }).join('');
-        this.names.forEach(function(item) {
-            this.element.querySelector(`input[name=${item.id}]`).addEventListener('change', this.handleChange.bind(this));
+        this.element.innerHTML = this.tpl(this.buttons);
+        Object.keys(this.buttons).forEach(function(name) {
+            const b = this.buttons[name];
+            this.element.querySelector('#'+ b.id)
+                .addEventListener('change', this.handleChange.bind(this));
         }, this);
     }
 
     handleChange(event) {
-        this.onChange(event.target.name, event.target.checked);
+        const checked = event.target.checked;
+        const name = event.target.name;
+        const parent = event.target.closest('.tl_checkbox_container');
+        parent.style.background = checked ? this.buttons[name].color : 'transparent';
+        this.onChange(name, checked);
+    }
+
+    tpl(buttons) {
+        const buttonsTpl = Object.keys(buttons).map(function(bName){
+            const b = this.buttons[bName];
+            return this.buttonTpl(b.id, b.name, b.color);
+        }, this);
+        return `<div class="tl_graphic_buttons">
+            <div class="tl_graphic_buttons_row">${buttonsTpl.join('')}</div>
+        </div>`;
+    }
+
+    buttonTpl(id, name, color) {
+        return `
+        <div class="tl_graphic_buttons_cell">
+            <div class="tl_graphic_button">
+                <div class="tl_checkbox_container" style="border-color: ${color};background: ${color};">
+                    <svg class="tl_scheckbox" viewBox="-295 358 78 78">
+                        <path class="tl_scheckbox_stroke" d="M-273.2,398.2l10,9.9 l22.4-22.3"></path>
+                    </svg>
+                    <input id="${id}" name="${name}" type="checkbox" class="tl_checkbox" checked="checked"></div>
+                <label class="tl_graphic_title" for="${id}">${name}</label></div>
+        </div>
+    `;
     }
 }
+
+
