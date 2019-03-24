251e460ace039b1d314c22f0500a71fc4ad8b9a2
a.gutnikov <a.gutnikov@corp.badoo.com>
2019-03-24 11:02:10 +0300

    tt, themes

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 rm -fr dist
-./node_modules/.bin/babel src -d dist
-cat dist/base.js >> dist/bundle.js
-#cat dist/theme.js >> dist/bundle.js
-cat dist/buttons.js >> dist/bundle.js
-cat dist/ruler.js >> dist/bundle.js
-cat dist/canvas.js >> dist/canvas.js
-cat dist/app.js >> dist/bundle.js
-./node_modules/.bin/terser --compress --mangle -- dist/bundle.js > dist/app.minified.js
+mkdir dist
+touch dist/bundle.js
+
+./node_modules/.bin/babel src/base.js >> dist/bundle.js
+./node_modules/.bin/babel src/theme.js >> dist/bundle.js
+./node_modules/.bin/babel src/buttons.js >> dist/bundle.js
+./node_modules/.bin/babel src/ruler.js >> dist/bundle.js
+./node_modules/.bin/babel src/canvas.js >> dist/bundle.js
+./node_modules/.bin/babel src/app.js >> dist/bundle.js
+./node_modules/.bin/terser --compress -- dist/bundle.js > dist/bundle.minified.js
 
